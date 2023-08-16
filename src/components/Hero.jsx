7be70ee7786a4e19a104084816273593a7dcf919
amrashour <a.ashour@msd-my.com>
2023-08-16 10:52:10 +0300

    added hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,6 +19,18 @@ const Hero = () => {
       </div>
       
       <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-4 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center p-2 items-start'> 
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
+              className='w-3 h-3 rounded-full mb-1 bg-secondary'
+            />
+          </div>
+        </a>  
+      </div>
     </section>
   )
 }
